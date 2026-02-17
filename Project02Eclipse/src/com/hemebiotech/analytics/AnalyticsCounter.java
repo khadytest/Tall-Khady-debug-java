57a747bd37b74e979a3d1f615bee4b13ee737390
Tall <tal@gmail.com>
2026-02-17 13:47:29 +0100

    modifier

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,42 +1,89 @@
 package com.hemebiotech.analytics;
 
+import javax.sound.sampled.Line;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.*;
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
+    private static int headacheCount = 0;
+    private static int rashCount = 0;
+    private static int pupilCount = 0;
+    private ISymptomReader reader;
+    private ISymptomWriter writer;
 
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
 
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    public List<String> getSymptoms() {
+        return reader.GetSymptoms();
+    }
 
-			line = reader.readLine();
-		}
+    public int addition(int a, int b){
+        return a+b;
+    }
 
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String element : symptoms) {
+            if (map.containsKey(element)) {
+                int occurence = map.get(element);
+                map.put(element, occurence+1);
+            } else {
+                map.put(element, 1);
+            }
+        }
+        return map;
+    }
+
+//    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms){
+//        Map<String, Integer> mapSymtoms = new HashMap<>();
+//        for (String elem : symptoms.keySet()){
+//            Integer value = symptoms.get(elem);
+//
+//        }
+//        return  mapSymtoms;
+//
+//    }
+    public void writeSymptoms(Map<String, Integer> symptoms){
+      this.writer.writeSymptoms(symptoms);
+    }
+
+
+
+
+
+    public static void main(String args[]) throws Exception {
+        FileReader readerObj = new FileReader("Project02Eclipse/symptoms.txt");
+        BufferedReader reader = new BufferedReader(readerObj);
+        String line = reader.readLine();
+
+        int i = 0;
+        int headCount = 0;
+        while (line != null) {
+            i++;
+            System.out.println("symptom from file: " + line);
+            if (line.equals("headache")) {
+                headCount++;
+                System.out.println("number of headaches: " + headCount);
+            } else if (line.equals("rush")) {
+                rashCount++;
+            } else if (line.contains("pupils")) {
+                pupilCount++;
+            }
+
+            line = reader.readLine();
+        }
+
+        FileWriter writer = new FileWriter("result.out");
+        writer.write("headache: " + headacheCount + "\n");
+        writer.write("rash: " + rashCount + "\n");
+        writer.write("dialated pupils: " + pupilCount + "\n");
+        writer.close();
+    }
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,26 @@
+package com.hemebiotech.analytics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        ISymptomReader iSymptomReader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+
+        ISymptomWriter iSymptomWriter = new WriteSymptomDataToFile("Project02Eclipse/write.txt", 2);
+
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(iSymptomReader, iSymptomWriter);
+        List<String> symptomList = analyticsCounter.getSymptoms();
+
+
+
+       Map<String, Integer> symptMap = analyticsCounter.countSymptoms(symptomList);
+
+       for(String key : symptMap.keySet()){
+           Integer value = symptMap.get(key);
+           System.out.println("key" + key + "value = " + value);
+       }
+
+    }
+}
